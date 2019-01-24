ed17e7d41a25058534c50597284328d864db8cae
Raghu Raghavan <dadra.raghu@live.com>
2019-01-24 15:44:01 -0800

    Logger init

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # logger
+
+## CLIENT-SIDE LOGGER
+
+### DESCRIPTION
+LOGGER is a javascript utility designed to extend functionality related to common logging methods in a browser environment related specifically to the "console" object exposed by the javascript run-time engine (v8 engine). Logger can be configured to handle posting specific log messages and / or unhandled expections to a backend service. 
+
+Logger specifically tracks errors that propogates to the window event handling system and include: 'Unhandledrejection' & 'error' events, which are supported by and in-spec with the w3c Document Object Model (DOM) Events Specifications.
+
+### [API]
+_npm install logger --save
+
+### Support
+
+- Node 
+- Browser (tested in IE 11, Chrome 30, Safari 8, FireFox 34)
--- node_build/config/directory.js
+++ node_build/config/directory.js
@@ -1,16 +1,21 @@
 const path = require('path')
 const join = path.join
-const root = join(path.resolve(__dirname),'../..')
+const root = join(path.resolve(__dirname), '../..')
 
 const directory = {
-    root:root,
-    dist : {
-        root : join(root, 'dist'),
-        'logger-min': join(root,'dist/logger-min.js'),
-        'logger' : join(root,'dist/logger.js')
+    root: root,
+    dist: {
+        root: join(root, 'dist'),
+        'browserLogger-min': join(root, 'dist/browserLogger-min.js'),
+        'browserLogger': join(root, 'dist/browserLogger.js')
     },
-    node_modules: {root : join(root,'node_modules')},
-    lib : {root : join (root, 'lib'), logger : join(root,'lib/logger.js')}
+    node_modules: {
+        root: join(root, 'node_modules')
+    },
+    lib: {
+        root: join(root, 'lib'),
+        browserLogger: join(root, 'lib/logger.js')
+    }
 }
 
 module.exports = directory
